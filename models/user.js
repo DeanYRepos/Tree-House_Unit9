4701f68768b3a36000ff08cf930a93e0715bf1ac
DeansRepos <54600923+DeanYRepos@users.noreply.github.com>
2021-04-08 19:10:41 -0500

    Added user and course models, and model association, on step 6 setting up user route

---
+++ models/user.js
@@ -0,0 +1,31 @@
+'use strict'
+const { Model, DataTypes } = require('sequelize');
+
+
+module.exports = (sequelize) => {
+    class User extends Model {}
+    User.init({
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        firstName: {
+            type: DataTypes.STRING,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+        },
+        emailAddress: {
+            type: DataTypes.STRING, 
+        },
+        password: {
+            type: DataTypes.STRING
+        }
+    }, { sequelize });
+
+    User.associate = (models) => {
+        User.hasMany(models.Course);
+    }
+    return User;
+}
